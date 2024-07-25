133d46bc4737ec7e776fcf56750055d1a33ad8ad
HajarLekrafi <hajar.lekrafi2@gmail.com>
2024-07-25 15:40:44 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ if uploaded_file is not None:
                                             fig_ville_cluster.add_annotation(
                                                 x=row['Cluster'],
                                                 y=row['Count'],
-                                                text=row['Ville'],
+                                                text=row['Ville_Nom'],
                                                 showarrow=True,
                                                 arrowhead=2,
                                                 ax=0,
