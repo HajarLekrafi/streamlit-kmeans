4fe050c30a6be8e56a763a06aa056c4cf3df5375
HajarLekrafi <hajar.lekrafi2@gmail.com>
2024-08-05 13:25:03 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -331,9 +331,9 @@ if uploaded_file is not None:
                                         st.markdown(f"""
                                             <div class="features">
                                                 <div class="feature">
-                                                    <p> Médiane: La médiane du montant est de {median_mnt:.2f}. Cela signifie que la moitié des propositions ont des montants inférieurs ou égaux à cette valeur.</Br>
-                                           Intervalle Interquartile (IQR) : {iqr:.2f}. C'est la différence entre le premier et le troisième quartile, montrant combien les montants sont dispersés autour de la médiane.</Br>
-                                          Montants Minimum et Maximum : Les montants varient de {min_mnt:.2f} à {max_mnt:.2f}, indiquant les valeurs les plus basses et les plus élevées dans ce groupe.</Br></p>
+                                                    <p> <strong>Médiane: </strong>La médiane du montant est de {median_mnt:.2f}. Cela signifie que la moitié des propositions ont des montants inférieurs ou égaux à cette valeur.</Br>
+                                           <strong>Intervalle Interquartile (IQR) : </strong>{iqr:.2f}. C'est la différence entre le premier et le troisième quartile, montrant combien les montants sont dispersés autour de la médiane.</Br>
+                                          <strong>Montants Minimum et Maximum : </strong>Les montants varient de {min_mnt:.2f} à {max_mnt:.2f}, indiquant les valeurs les plus basses et les plus élevées dans ce groupe.</Br></p>
                                             
                                    <p> En résumé, les boxplots montrent comment les montants des propositions sont distribués dans chaque groupe (cluster). </Br>
                                         Les différences entre les groupes peuvent indiquer des variations importantes dans les montants, ce qui peut nous aider à comprendre les caractéristiques des propositions dans chaque groupe et à repérer les valeurs extrêmes.</p>
